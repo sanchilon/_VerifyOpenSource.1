c79c8f187318e5bb42867998e0e41424ff7bb482
sanchilon <sandeep.chiluveru@ontario.ca>
2022-01-07 14:18:49 -0500

    modified transform.sh

--- scripts/_transform.sh
+++ scripts/_transform.sh
@@ -29,8 +29,8 @@ function replace_inside_files {
     replace_with=$2
     files=$(ggrep --exclude-dir=builds \
                   --exclude-dir=scripts/sources \
-                  --exclude=prepare.pl \
                   --exclude=transform.sh \
+                  --exclude=prepare.pl \
                   --exclude=README.md \
                   --exclude=README-fr.md \
                   --exclude-dir=node_modules \
@@ -55,7 +55,57 @@ function replace {
     rename_files "$find" "$replace_with" "1"
 }
 BACKUP_FOLDER=".git-backup-$(date "+%Y%m%d%H%M%S")"
-mv .git "../${BACKUP_FOLDER}"
+mv ./OntarioVerify/.git "../${BACKUP_FOLDER}"
+##########################
+set +f
+rm -Rf ./OntarioVerify/.github
+rm ./OntarioVerify/*.sh
+rm ./OntarioVerify/Contributing.md
+rm -Rf ./OntarioVerify/src/__mocks__/trust
+cp ./scripts/sources/README.md ./OntarioVerify/
+cp ./scripts/sources/README-fr.md ./OntarioVerify/
+cp ./scripts/sources/tsconfig.json ./OntarioVerify/
+cp ./scripts/sources/.env.template ./OntarioVerify/
+cp ./scripts/sources/LICENSE.txt ./OntarioVerify/
+cp ./scripts/sources/package.json ./OntarioVerify/
+cp ./scripts/sources/src/__mocks/*.json ./OntarioVerify/src/__mocks__/
+cp ./scripts/sources/src/assets/images/* ./OntarioVerify/src/assets/images/
+cp ./scripts/sources/src/containers/home/*.tsx ./OntarioVerify/src/containers/home/
+cp ./scripts/sources/android/app/*.json ./OntarioVerify/android/app/
+cp -R ./scripts/sources/android/app/src/main/res/mipmap-hdpi/*.png ./OntarioVerify/android/app/src/main/res/mipmap-hdpi/
+cp -R ./scripts/sources/android/app/src/main/res/mipmap-ldpi/*.png ./OntarioVerify/android/app/src/main/res/mipmap-ldpi/
+cp -R ./scripts/sources/android/app/src/main/res/mipmap-mdpi/*.png ./OntarioVerify/android/app/src/main/res/mipmap-mdpi/
+cp -R ./scripts/sources/android/app/src/main/res/mipmap-xhdpi/*.png ./OntarioVerify/android/app/src/main/res/mipmap-xhdpi/
+cp -R ./scripts/sources/android/app/src/main/res/mipmap-xxhdpi/*.png ./OntarioVerify/android/app/src/main/res/mipmap-xxhdpi/
+cp -R ./scripts/sources/android/app/src/main/res/mipmap-xxxhdpi/*.png ./OntarioVerify/android/app/src/main/res/mipmap-xxxhdpi/
+cp -R ./scripts/sources/ios/OpenVerify/Images.xcassets ./OntarioVerify/ios/OpenVerify/
+cp ./scripts/sources/ios/*.plist ./OntarioVerify/ios/
+sed -i "" "s~Open Verify~VérifOuverte~g" ./OntarioVerify/src/translations/fr.json
+
+STAR_COMMENT_LICENSE=$(cat <<EOF
+/*
+   Copyright 2021 Queen’s Printer for Ontario
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+EOF
+)
+set -f
+
+cp -R "../${BACKUP_FOLDER}" ./OntarioVerify/.git
+
+##################
+
 replace "Verify Ontario" "Open Verify"
 replace "VerifyOntario" "OpenVerify"
 replace "OntarioVerify" "OpenVerify"
@@ -79,54 +129,15 @@ sed -i "" "s~/android/app/google-services.json~~g" ".gitignore"
 rename_files "ca" "openverify" ""
 rename_files "ontario" "replace" ""
 rename_files "verify" "me" ""
-set +f
-rm -Rf ./OpenVerify/.github
-rm ./OpenVerify/*.sh
-rm ./OpenVerify/Contributing.md
-rm -Rf ./OpenVerify/src/__mocks__/trust
-cp scripts/sources/README.md ./OpenVerify
-cp scripts/sources/README-fr.md ./OpenVerify
-cp scripts/sources/tsconfig.json ./OpenVerify
-cp scripts/sources/.env.template ./OpenVerify
-cp scripts/sources/LICENSE.txt ./OpenVerify
-cp scripts/sources/package.json ./OpenVerify
-cp scripts/sources/src/__mocks/*.json ./OpenVerify/src/__mocks__/
-cp scripts/sources/src/assets/images/* ./OpenVerify/src/assets/images/
-cp scripts/sources/src/containers/home/*.tsx ./OpenVerify/src/containers/home/
-cp scripts/sources/android/app/*.json ./OpenVerify/android/app/
-cp -R scripts/sources/android/app/src/main/res/mipmap-hdpi/*.png ./OpenVerify/android/app/src/main/res/mipmap-hdpi/
-cp -R scripts/sources/android/app/src/main/res/mipmap-ldpi/*.png ./OpenVerify/android/app/src/main/res/mipmap-ldpi/
-cp -R scripts/sources/android/app/src/main/res/mipmap-mdpi/*.png ./OpenVerify/android/app/src/main/res/mipmap-mdpi/
-cp -R scripts/sources/android/app/src/main/res/mipmap-xhdpi/*.png ./OpenVerify/android/app/src/main/res/mipmap-xhdpi/
-cp -R scripts/sources/android/app/src/main/res/mipmap-xxhdpi/*.png ./OpenVerify/android/app/src/main/res/mipmap-xxhdpi/
-cp -R scripts/sources/android/app/src/main/res/mipmap-xxxhdpi/*.png ./OpenVerify/android/app/src/main/res/mipmap-xxxhdpi/
-cp -R scripts/sources/ios/OpenVerify/Images.xcassets ./OpenVerify/ios/OpenVerify/
-cp scripts/sources/ios/*.plist ./OpenVerify/ios/
-sed -i "" "s~Open Verify~VérifOuverte~g" src/translations/fr.json
 
-STAR_COMMENT_LICENSE=$(cat <<EOF
-/*
-   Copyright 2021 Queen’s Printer for Ontario
+##################
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-EOF
-)
-set -f
+##################
 files=$(ggrep '--exclude-dir=android/builds' \
                 '--exclude-dir=scripts/sources' \
-                '--exclude=prepare.pl' \
                 '--exclude=transform.sh' \
+                ' --exclude=prepare.pl' \
                 '--exclude-dir=node_modules' \
                 '--exclude-dir=ios/Pods' \
                 '--exclude-dir=.git' \
@@ -143,7 +154,7 @@ if [[ $files ]]; then
     done
 fi
 
-cp -R "../${BACKUP_FOLDER}" ./OpenVerify/.git
+
 
 #yarn
 #yarn prettier -w --bracket-same-line 'src' '!src/__mocks__'
